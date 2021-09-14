146105a094619807bbe485711ead9d65cc4aea46
junyong <thwnsdyd@gmail.com>
2021-09-14 15:06:49 +0900

    개인체형과 연령별 평균 체형과 비교, 개인 체형 렌더링 부분 까지 구현

--- main.js
+++ main.js
@@ -1,264 +1,115 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const html = require("http");
-const path = require("path");
-const url = require("url");
-const ejs = require("ejs");
-const app = express();
-const port = 3300;
-const mysql = require("mysql2");
-const static = require("serve-static");
+var express = require("express");
+var app = express();
+var router = express.Router();
+var fs = require("fs");
+var mysql = require("mysql2");
+var static = require('serve-static');
+var bodyParser = require("body-parser");
 
-const db = mysql.createConnection({
+//터미널 실행
+var spawn = require("child_process").spawn;
+var exec = require('child_process').exec;
+//upload 모듈
+var multer = require('multer');
+var upload = multer({dest: "sample_data/input/"});
+
+//db연결
+var db = mysql.createConnection({
   host: "bodywebdb.cnrtoyi1tcyy.ap-northeast-2.rds.amazonaws.com",
   user: "root",
   password: "grom0419",
   database: "BodyWebDB",
-  multipleStatements: true,
+  multipleStatements: true
 });
 db.connect();
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "template/insertBodyInfo.html"))
-);
-app.post("/insertBodyinfo", function (request, response) {
-  // console.log(request);
-  let insertSql =
-    "INSERT INTO userBodyData (gender,age,height,weight,inseam,armLength,shoulderLength,chestCirc,waistCirc,hipCirc,thighCirc,armCirc,calfCirc,headWidth) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-  var post = request.body;
-  var gender_ = post.gender;
-  var age_ = post.age;
+//배경사진 업로드
+app.get("/", function (req, res) {
+  res.render("uploadBackground");
+  res.end();
+});
+
+// 사람사진 업로드
+app.get("/uploadHuman", function (req, res) {
+  res.render("uploadHuman");
+});
+
+//키입력
+app.get('/insertHeight', function (req, res) {
+  res.render('insertHeight')
+})
+
+//db키입력
+app.post('/insertHeight', function (req, res) {
+  var post = req.body;
   var height_ = post.height;
-  var weight_ = post.weight;
-  var inseam_ = post.inseam;
-  var armLength_ = post.armLength;
-  var shoulderLength_ = post.shoulderLength;
-  var chestCirc_ = post.chestCirc;
-  var waistCirc_ = post.waistCirc;
-  var hipCirc_ = post.hipCirc;
-  var thighCirc_ = post.thighCirc;
-  var armCirc_ = post.armCirc;
-  var calfCirc_ = post.calfCirc;
-  var headWidth_ = post.headWidth;
-  db.query(
-    insertSql,
-    [
-      gender_,
-      age_,
-      height_,
-      weight_,
-      inseam_,
-      armLength_,
-      shoulderLength_,
-      chestCirc_,
-      waistCirc_,
-      hipCirc_,
-      thighCirc_,
-      armCirc_,
-      calfCirc_,
-      headWidth_,
-    ],
-    function (error, result) {
-      if (error) {
-        console.error("error : ", error);
-      }
-    }
-  );
-
-  response.writeHead(302, { Location: "/insertExerInfo" });
-  response.end();
-});
-app.get("/insertexerInfo", function (request, response) {
-  var result = [];
-  console.log(result);
-  var getinfo = function (callback) {
-    db.query("SELECT * FROM userBodyData", function (err, res, fields) {
-      if (err) {
-        return callback(err);
-      }
-      if (res.length) {
-        for (var i = 0; i < res.length; i++) {
-          result.push(res[i]);
-        }
-      }
-      callback(null, result);
-    });
-  };
-  console.log("callfunction");
-  getinfo(function (err, result) {
+  console.log(height_)
+  var insertsql = "INSERT INTO userHeight (height) VALUES(?)"
+  db.query(insertsql, [height_], function (err, res, fields) {
     if (err) {
-      console.log("err");
-    } else {
-      var gender_ = result[0].gender;
-      console.log(result[0].gender);
-      if (result[0].gender == "male") {
-        // var selectInsertModelSql =
-        //     "INSERT INTO objName(filename) SELECT maleData.id  FROM maleData WHERE (age = ? and height = ? and weight = ? and inseam = ? and armLength = ? and shoulderLength = ? and chestCirc = ? and waistCirc = ? and hipCirc = ? and thighCirc = ? and armCirc = ? and calfCirc = ? and headWidth = ?)";
-        db.query(
-          "INSERT INTO objName(filename) SELECT maleData.id  FROM maleData WHERE (age = ? and height = ? and weight = ? and inseam = ? and armLength = ? and shoulderLength = ? and chestCirc = ? and waistCirc = ? and hipCirc = ? and thighCirc = ? and armCirc = ? and calfCirc = ? and headWidth = ?)",
-          [
-            result[0].age,
-            result[0].height,
-            result[0].weight,
-            result[0].inseam,
-            result[0].armLength,
-            result[0].shoulderLength,
-            result[0].chestCirc,
-            result[0].waistCirc,
-            result[0].hipCirc,
-            result[0].thighCirc,
-            result[0].armCirc,
-            result[0].calfCirc,
-            result[0].headWidth,
-          ],
-          function (err, result) {
-            if (!err) {
-              // console.log(result);
-            } else {
-              console.error(err);
-            }
-          }
-        );
-      } else {
-        // var selectInsertFModelSql =
-        //     "INSERT INTO objName(filename) SELECT femaleData.id FROM femaleData WHERE (age = ? and height = ? and weight = ? and inseam = ? and armLength = ? and shoulderLength = ? and chestCirc = ? and waistCirc = ? and hipCirc = ? and thighCirc = ? and armCirc = ? and calfCirc = ? and headWidth = ?)"
-        db.query(
-          "INSERT INTO objName(filename) SELECT femaleData.id FROM femaleData WHERE (age = ? and height = ? and weight = ? and inseam = ? and armLength = ? and shoulderLength = ? and chestCirc = ? and waistCirc = ? and hipCirc = ? and thighCirc = ? and armCirc = ? and calfCirc = ? and headWidth = ?)",
-          [
-            result[0].age,
-            result[0].height,
-            result[0].weight,
-            result[0].inseam,
-            result[0].armLength,
-            result[0].shoulderLength,
-            result[0].chestCirc,
-            result[0].waistCirc,
-            result[0].hipCirc,
-            result[0].thighCirc,
-            result[0].armCirc,
-            result[0].calfCirc,
-            result[0].headWidth,
-          ],
-          function (err, result) {
-            if (!err) {
-              console.log(result);
-            } else {
-              console.error(err);
-            }
-          }
-        );
-      }
+      console.error(err);
     }
-  });
+  })
+  res.writeHead(302, {Location: '/measurements'})
+  res.end()
+})
 
-  response.writeHead(302, { Location: "/renderingModel" });
-  response.end();
-});
-
-app.use(static(path.join(__dirname, "model/obj")));
-app.get("/renderingModel", function (request, response) {
-  var filename = [];
-  var getInformationFromDB = function (callback) {
-    db.query("SELECT filename FROM objName", function (err, res, fields) {
-      if (err) return callback(err);
-      if (res.length) {
-        for (var i = 0; i < res.length; i++) {
-          filename.push(res[i]);
-        }
-      }
-      callback(null, filename);
-    });
+// 신체치수 계산
+app.get('/measurements', function (req, res) {
+  var result = [];
+  console.log(result)
+  var getinfo = function (callback) {
+    db.query("SELECT * FROM userHeight",
+        function (err, res, fields) {
+          if (err) {
+            return callback(err);
+          }
+          if (res.length) {
+            for (var i = 0; i < res.length; i++) {
+              result.push(res[i]);
+            }
+          }
+          callback(null, result);
+        });
   };
-
-  getInformationFromDB(function (err, result) {
+  console.log("callfunction")
+  getinfo(function (err, result) {
     if (err) {
       console.error(err);
     } else {
-      console.log(filename);
-      console.log(filename[0].filename);
-      var html = `
-        <!DOCTYPE html>
-<html lang="ko">
-<head>
-  <meta charset="UTF-8"/>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Female Body Changing Simulator</title>
-</head>
+      var height_ = result[0].height
+      var option = {
+        shell: '/bin/zsh',
+        windowshide: true
+      }
 
-<body ontouchstart="">
-<!-- 제목 -->
-<h1>신체 시뮬레이터</h1>
-<!-- 성별 선택 -->
+      var command =
+          `conda run -n bodyweb python3 calc.py -i sample_data/input/so.jpg -ht ${height_}`
 
-<!-- 신체 정보 및 운동정보 입력창 -->
-
-<script type="module">
-  import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.122.0/build/three.module.js";
-  import {OrbitControls} from "https://cdn.jsdelivr.net/npm/three@0.122.0/examples/jsm/controls/OrbitControls.js";
-  import {OBJLoader} from "https://cdn.jsdelivr.net/npm/three@0.122.0/examples/jsm/loaders/OBJLoader.js";
-  // import {APP} from "../js/app.js";
-  import {MeshNormalMaterial} from "https://cdn.jsdelivr.net/npm/three@0.122.0/src/materials/MeshNormalMaterial.js";
-
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera(75, 1 / 2, 1, 5000);
-  camera.position.x = 0;
-  camera.position.y = 2200;
-  camera.position.z = -1600;
-
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth / 2, window.innerHeight);
-  renderer.setClearColor(0xffffff);
-  renderer.border;
-  document.body.appendChild(renderer.domElement);
-
-  var controls = new OrbitControls(camera, renderer.domElement);
-  controls.update();
-
-  animate();
-
-  function animate() {
-    requestAnimationFrame(animate);
-    controls.update();
-    renderer.render(scene, camera);
-  }
-
-  var loader = new OBJLoader();
-  var material = new MeshNormalMaterial();
-  // loader.setMaterials(material);
-  var object;
-  var body;
-
-  console.log(name)
-  loader.load("/"+${filename[0].filename}+".obj",
-      function (object) {
-        object.traverse(function (body) {
-          if (body instanceof THREE.Mesh) {
-            body.material = material;
-            body.rotation.x = (Math.PI / 360) * -100;
-            scene.add(body);
+      exec(command,
+          function (error, stdout, stderr) {
+            console.log(error)
+            console.log(stdout)
+            console.log(stderr)
           }
-        });
-
-        scene.add(object);
-        return;
-      });
-
-  // 크기측정 함수부분
-</script>
-</body>
-</html>
-        `;
+      )
+      ;
     }
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end(html);
-  });
+  })
+  console.log('완료')
+})
+
+// 운동 입력
+
+//연결
+var port = 3400;
+app.listen(port, function () {
+  var dir = "sample_data/input/";
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+  console.log("server on! http://localhost:" + port);
 });
-
-// app.get("/renderingModel", function (req, res) {
-//   res.sendFile(path.join(__dirname + "/template/renderingModel.html"));
-// });
-
-app.listen(port);
